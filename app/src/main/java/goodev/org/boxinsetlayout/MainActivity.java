03e3664184efe3ba40bd9c3c7bddc6c04251d2c3
yfcheng <yfcheng@mobvoi.com>
2015-05-18 10:36:53 +0800

    Initial commit

---
+++ app/src/main/java/goodev/org/boxinsetlayout/MainActivity.java
@@ -0,0 +1,15 @@
+package goodev.org.boxinsetlayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.round_activity_main);
+    }
+}
